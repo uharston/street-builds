6657ffd8c0455d5b9e09b729ad270b5829a1200f
uharston <uharston@gmail.com>
2020-07-26 17:49:42 -0500

    User and Session Routes and Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users', to: 'users#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
 end
