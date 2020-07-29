9932aeb0afe9d25cec47ffaf0dbbbba640b3ac61
uharston <uharston@gmail.com>
2020-07-29 10:14:20 -0500

    Deleted Old Code

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,4 @@
 class Board < ApplicationRecord
-    # belongs_to :project_car_admirer, class_name: "User"
-    belongs_to :user#, class_name: "User"
-    has_and_belongs_to_many :project_cars#admired_project_cars, class_name: "ProjectCar"
+    belongs_to :user
+    has_and_belongs_to_many :project_cars
 end
