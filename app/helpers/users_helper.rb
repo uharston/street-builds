22393c49c342f4993d02211ddc3ed8e7caed4151
uharston <uharston@gmail.com>
2020-08-02 19:08:22 -0500

    User#show view helper method #edit_or_save

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -22,4 +22,14 @@ module UsersHelper
             @user.first_name + "'s Garage"
         end
     end 
+
+    
+    def edit_or_save(user)
+         if current_user == user.owner 
+            link_to "Edit Your Build", edit_car_path(user), class: "w3-large w3-button w3-red w3-round-large w3-left"          
+        else 
+            link_to "Save This Ride", new_car_pin_path(user), class: "w3-large w3-button w3-green w3-round-large w3-left"
+        end 
+    end
+
 end
