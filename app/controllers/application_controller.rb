b8e0be4de1d842d777fc2587a11b9076a1274d2a
uharston <uharston@gmail.com>
2020-10-24 14:40:29 -0500

    Authorized User Method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-    # before_action :check_user 
     helper_method :current_user, :check_user
 
     def check_user 
         redirect_to root_path unless logged_in?
     end 
+
+    def authorized_user 
+        redirect_to cars_path unless @user == current_use
+    end 
+
     def logged_in? 
         !!current_user
     end 
